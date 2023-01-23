77c8bd0124ffbe67cfd7270eeef4447ffcb4aa2c
erto2000 <ertugrulkeskin2000@hotmail.com>
2023-01-23 20:23:01 +0300

    Table Generator bug fix

--- Simulation/Table_Generator_Test.v
+++ Simulation/Table_Generator_Test.v
@@ -11,7 +11,7 @@ module Table_Generator_Test;
     reg is_new_coefficient = 1'b0;
     
     //Outputs of the module
-    wire [2047:0] value;
+    wire [64*8-1:0] value;
     wire valid;
     
     //Change clock with given period
@@ -22,7 +22,8 @@ module Table_Generator_Test;
         .rst(rst),
         .is_new_coefficient(is_new_coefficient),
         .r_value(r_value),
-        .value(calue),
+        .coefficient(coefficient),
+        .value(value),
         .valid(valid)
     );
 
@@ -33,33 +34,25 @@ module Table_Generator_Test;
         @(posedge clk)
         rst <= 1'b0;
         
-        //DETAILED EXAMPLES
         //DC coefficient is first element
-        give_bit_sequence(4'b0, 8'b10101010);
+        give_bit_sequence(4'b0000, 8'b10101010);
         
-        //
+        //AC coefficients
         give_bit_sequence(4'b0110, 8'b11110000);
-        
-        //
         give_bit_sequence(4'b1010, 8'b11001100);
-        
-        //
         give_bit_sequence(4'b0011, 8'b11010001);
 
+        is_new_coefficient <= 1'b0;
     end
     
     //Gives bit sequence into table generator
-    task give_bit_sequence(input reg[3:0] bit_sequence, input reg[7:0]  bit_seq_2); 
+    task give_bit_sequence(input reg[3:0] r_value_input, input reg[7:0] coefficient_input); 
         integer i;
     begin
-        r_value <= bit_sequence;
-        coefficient <= bit_seq_2;
+        r_value <= r_value_input;
+        coefficient <= coefficient_input;
         is_new_coefficient <= 1'b1;
-        #10;
         @(posedge clk);
-        r_value <= 3'b0;
-        coefficient <= 7'b0;
-        is_new_coefficient <= 1'b0;
     end
     endtask    
     
