d1d4e54292a82b439f3ffee3fe98c3398968412d
circuitmaster <45884610+circuitmaster@users.noreply.github.com>
2023-01-25 00:04:22 +0300

    Table Generator Dequant
    Dequantization multiplications added to table generator hardware

--- Design/Table_Generator.v
+++ Design/Table_Generator.v
@@ -18,9 +18,13 @@ module Table_Generator(
     
     //Zig Zag index parameters
     parameter [0:64*8-1] params = {8'd0,8'd1,8'd8,8'd16,8'd9,8'd2,8'd3,8'd10,8'd17,8'd24,8'd32,8'd25,8'd18,8'd11,8'd4,8'd5,8'd12,8'd19,8'd26,8'd33,8'd40,8'd48,8'd41,8'd34,8'd27,8'd20,8'd13,8'd6,8'd7,8'd14,8'd21,8'd28,8'd35,8'd42,8'd49,8'd56,8'd57,8'd50,8'd43,8'd36,8'd29,8'd22,8'd15,8'd23,8'd30,8'd37,8'd44,8'd51,8'd58,8'd59,8'd52,8'd45,8'd38,8'd31,8'd39,8'd46,8'd53,8'd60,8'd61,8'd54,8'd47,8'd55,8'd62,8'd63};
+    parameter [0:64*8-1] params_quant = {8'd16,8'd11,8'd10,8'd16,8'd24,8'd40,8'd51,8'd61,8'd12,8'd12,8'd14,8'd19,8'd26,8'd58,8'd60,8'd55,8'd14,8'd13,8'd16,8'd24,8'd40,8'd57,8'd69,8'd56,8'd14,8'd17,8'd22,8'd29,8'd51,8'd87,8'd80,8'd62,8'd18,8'd22,8'd37,8'd56,8'd68,8'd109,8'd103,8'd77,8'd24,8'd35,8'd55,8'd64,8'd81,8'd104,8'd113,8'd92,8'd49,8'd64,8'd78,8'd87,8'd103,8'd121,8'd120,8'd101,8'd72,8'd92,8'd95,8'd98,8'd112,8'd100,8'd103,8'd99};
     
     //Zig Zag index memory
     reg[7:0] memory [0:63];
+    
+    //Quantization coefficients memory
+    reg[7:0] memory_quant [0:63];
     
     //Table memory 
     reg[7:0] zig_zag_matrix [0:63];
@@ -36,6 +40,7 @@ module Table_Generator(
     initial begin
         for(i=0; i<64; i=i+1) begin
             memory[i] <= params[i*8 +: 8];
+            memory_quant[i] <= params_quant[i*8 +: 8];
             zig_zag_matrix[i] <= 8'd0;
         end
     end
@@ -45,7 +50,7 @@ module Table_Generator(
         zig_zag_index <= memory[counter+r_value];
     
         for(i=0; i<64; i = i + 1) begin
-            value[i*8 +: 8] <= zig_zag_matrix[i];
+            value[i*8 +: 8] <= zig_zag_matrix[i]*memory_quant[i];
         end
     end 
 
