6c7b3f70a5edc77f7a1e5433d9c8a61e84c7dc66
circuitmaster <45884610+circuitmaster@users.noreply.github.com>
2023-02-25 23:23:41 +0300

    idct complete
    fully pipelined and fully parallelized idct8x8 design, finally a pipeline stage added between row idct and column idct modules.

--- Design/idct_complete.v
+++ Design/idct_complete.v
@@ -30,12 +30,20 @@ output  m_valid
     );
     wire [2047:0] temp, temp2;
     wire m_valid_middle;
+    reg [2047:0] temp_reg;
+    reg m_valid_middle_reg;
     IDCT_top idct_x(clk, rst, {24'b0, data_in[63:56], 24'b0, data_in[55:48], 24'b0, data_in[47:40], 24'b0, data_in[39:32], 24'b0, data_in[31:24], 24'b0, data_in[23:16], 24'b0, data_in[15:8], 24'b0, data_in[7:0]}, s_valid, 4'd11, temp, m_valid_middle);
-    IDCT_top idct_y(clk, rst, temp, m_valid_middle, 4'd15, temp2, m_valid);
+    IDCT_top idct_y(clk, rst, temp_reg, m_valid_middle_reg, 4'd15, temp2, m_valid);
     genvar i;
     for(i = 0; i<63; i = i + 1)
     begin
         assign data_out[7+i*8:i*8] = temp2[7+i*32:i*32]; 
+    end
+    
+    always @(posedge clk)
+    begin
+        temp_reg <= temp;
+        m_valid_middle_reg <= m_valid_middle;
     end
     
 endmodule
